9735ea3eacd0f2d2e0571142392784bfbf541b24
Arturo Vithe <47705254+Vithe123@users.noreply.github.com>
2022-01-31 17:11:35 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot as plt
+from tkinter import *
+
+
+plt.plot(2,3, marker="o", color="red")
+
+plt.ylim([-5,5])
+plt.xlim([-5,5])
+plt.show()
+
+
+#canvas = Canvas(width=400, height=300, bg='white')
+#canvas.pack(expand=YES, fill=BOTH)
+#canvas.create_line(10, 10, 80, 80)
+#canvas.create_line(10, 80, 80, 10)
+
+#mainloop()
